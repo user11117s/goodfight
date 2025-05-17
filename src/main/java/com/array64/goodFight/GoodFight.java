534f01ed224dac2386cdc43544753fa8723f65b9
ARNAV <arnavk.math@gmail.com>
2025-05-17 10:03:58 -0700

    Initial commit

---
+++ src/main/java/com/array64/goodFight/GoodFight.java
@@ -0,0 +1,17 @@
+package com.array64.goodFight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoodFight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
